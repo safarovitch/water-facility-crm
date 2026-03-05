e101cb2385f4530b6e125427e9a9338fccb29b52
safarovitch_jr <r.safarovitch@gmail.com>
2026-03-06 03:15:30 +0500

    Added asterisk

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -7,23 +7,25 @@ use Illuminate\Database\Seeder;
 
 class UsersSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        $user = [
-            'name' => 'Admin User',
-            'email' => 'r.safarovitch@gmail.com',
-            'phone' => '+992884238383',
-            'password' => bcrypt('password'),
-            'status' => \App\Enums\UserStatus::Active,
-        ];
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $user = [
+      'name' => 'Admin User',
+      'email' => 'r.safarovitch@gmail.com',
+      'phone' => '+992884238383',
+      'password' => bcrypt('password'),
+      'status' => \App\Enums\UserStatus::Active,
+      'sip_extension' => '1001',
+      'sip_password' => '08230a0d9912bbdb',
+    ];
 
-        $user = \App\Models\User::firstOrCreate(['email' => $user['email']], $user);
+    $user = \App\Models\User::updateOrCreate(['email' => $user['email']], $user);
 
-        if ($user->roles()->count() === 0) {
-            $user->assignRole('Admin');
-        }
+    if ($user->roles()->count() === 0) {
+      $user->assignRole('Admin');
     }
+  }
 }
